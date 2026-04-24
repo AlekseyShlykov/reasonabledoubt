d58320b58fd55f24fc6c1fbe840228065a32aa8d
Aleksei Shlykov <shlikovalexei@gmail.com>
2026-04-24 10:55:22 +0200

    ver2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import Script from 'next/script';
 import '@/styles/globals.css';
 
 const GA_MEASUREMENT_ID = 'G-1VL1RR3W2F';
+const FB_PIXEL_ID = '4281871595291721';
 
 /** Same rules as `next.config.js` so OG/Twitter absolute URLs match GitHub Pages / subpath deploys. */
 function publicBasePath(): string {
@@ -91,6 +92,31 @@ export default function RootLayout({
             gtag('config', '${GA_MEASUREMENT_ID}');
           `}
         </Script>
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${FB_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          {/* Facebook Pixel fallback; next/image is not applicable here */}
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            alt=""
+            height={1}
+            width={1}
+            style={{ display: 'none' }}
+            src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+          />
+        </noscript>
       </body>
     </html>
   );
